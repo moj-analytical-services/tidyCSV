1ef92e50780eb8811dbda4a38145ea994101ce2d
phillip hall <philip.hall@justice.gsi.gov.uk>
2019-12-10 14:38:54 +0000

    Added comments to code.

--- R/probation-caseload.R
+++ R/probation-caseload.R
@@ -1,4 +1,4 @@
-#' Convert a Probation Caseload CSV to Tidy format
+#' Convert a Probation Caseload CSV to Tidy format data frame
 #'
 #' @param file Path to the input file (CSV)
 #'
@@ -6,9 +6,14 @@
 #' @export
 probationCaseload <- function(file) {
 
+  # Read in the data, specifying the rows and columns to keep and variable names.
+  # Skip the first two lines which contain notes on the file.
+
   data <- read.csv(file,skip=2,stringsAsFactors = FALSE)
   data <- data[1:5]
   names(data) <- c("probation_area","supervision_type","sex","date","value")
+
+  # Convert date format and add extra varaibles describing the value column.
 
   data <- within(data, {
     start_date <- as.Date(date,"%d/%m/%Y")
@@ -16,6 +21,9 @@ probationCaseload <- function(file) {
     value_description <- "Offenders supervised by the Probation Service"
     value_type <- "count"
   })
+
+  # Use the two lookups stored in data/sysdata.rda to add additional detail.
+  # Includes geographic codes and supervision types that are inconsistent between years.
 
   lookup <- tidyCSV::probation_areas
 
@@ -25,6 +33,8 @@ probationCaseload <- function(file) {
 
   data <- merge(data,lookup,lookup,by.x="supervision_type",by.y="supervision_type_input",
                 suffixes=c("_old","_new"))
+
+  # Keep the final variable set.
 
   data <- dplyr::select(data,start_date,end_date,probation_area,nps_crc,nps_region,CTRY18CD,CTRY18NM,
                  sex,supervision_type = supervision_type_new,value_description,value_type,value)
--- R/probation-starts.R
+++ R/probation-starts.R
@@ -1,4 +1,4 @@
-#' Convert a Probation Starts CSV to Tidy format
+#' Convert a Probation Starts CSV to Tidy format data frame
 #'
 #' @param file Path to the input file (CSV)
 #'
@@ -6,16 +6,25 @@
 #' @export
 probationStarts <- function(file) {
 
+  # Read in the data, specifying the rows and columns to keep and variable names.
+  # Skip the first two lines which contain notes on the file.
+
   data <- read.csv(file,skip=3,stringsAsFactors = FALSE)
   data <- data[1:5]
   names(data) <- c("nps_region","supervision_type","sex","date","value")
 
+  # Convert date format and add extra varaibles describing the value column.
+
   data <- within(data, {
-    start_date <- as.Date(date,"%d/%m/%Y")
-    end_date <- as.Date(date,"%d/%m/%Y")
+    start_date <- as.Date(paste0("1-",str_sub(date,1,3),"-",str_sub(date,9,12)),format = "%d-%b-%Y")
+    end_date <- as.Date(paste0("1-",str_sub(date,5,7),"-",str_sub(date,9,12)),format = "%d-%b-%Y")
+    lubridate::day(end_date) <- lubridate::days_in_month(end_date)
     value_description <- "Offenders starting supervision by the Probation Service"
     value_type <- "count"
   })
+
+  # Use the two lookups stored in data/sysdata.rda to add additional detail.
+  # Includes geographic codes and supervision types that are inconsistent between years.
 
   lookup <- tidyCSV::probation_areas[,3:5]
 
@@ -25,6 +34,8 @@ probationStarts <- function(file) {
 
   data <- merge(data,lookup,lookup,by.x="supervision_type",by.y="supervision_type_input",
                 suffixes=c("_old","_new"))
+
+  # Keep the final variable set.
 
   data <- dplyr::select(data,start_date,end_date,nps_region,CTRY18CD,CTRY18NM,
                  sex,supervision_type = supervision_type_new,value_description,value_type,value)
